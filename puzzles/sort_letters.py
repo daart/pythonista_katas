8805f1ab95120edca39b702fd051b310329208d5
Artem Kuzko <artem.kuzko@datarobot.com>
2020-07-27 10:29:14 +0300

    updated sort letters solution with 1 more

--- puzzles/sort_letters.py
+++ puzzles/sort_letters.py
@@ -19,16 +19,23 @@ def sort_letters(text):
     
     if not len(text): return text
     
-    # noob solution
-    char_dict = OrderedDict();
-    
+    # great solution
+    frequency_by_char = dict();
     for char in text:
-        val = char_dict[char] if char in char_dict else 0
-        char_dict[char] = val + 1
+        val = frequency_by_char[char] if char in frequency_by_char else 0
+        frequency_by_char[char] = val + 1
     
-    sorted_char_dict = sorted(char_dict.items(), key=lambda char: char[1], reverse=True)
+    index_by_char = dict()
+    for index, char in enumerate(text):
+        if char not in index_by_char:
+            index_by_char[char] = index
     
-    return ''.join([key * val for key, val in sorted_char_dict]) 
+    triplets = [
+        [freq, -index_by_char[char], char] 
+        for char, freq in frequency_by_char.items()
+    ]
+    
+    return ''.join([(char * freq) for freq, index, char in sorted(triplets, reverse=True)])
     
     # since sorting is the heavies operation here O(n logn)
     
@@ -37,10 +44,52 @@ def sort_letters(text):
     # char_collection = Counter(text).most_common()
     
     # return ''.join([key * val for key, val in char_collection])
-    
-test1 = sort_letters('aaadccccbeefffff')
 
-print(test1)
+def sort_letters_2(text):
+    """
+    >>> sort_letters('aaabccccdeefffff')
+    'fffffccccaaaeebd'
+    >>> sort_letters('aaadccccbeefffff')
+    'fffffccccaaaeedb'
+    >>> sort_letters('abcdefghijklmnop')
+    'abcdefghijklmnop'
+    >>> sort_letters('')
+    ''
+    >>> sort_letters('aba')
+    'aab'
+    >>> sort_letters('abcabccba')
+    'aaabbbccc'
+    """
+    # only applicable for python 3.6+ version
+    char_collection = Counter(text).most_common()
+    return ''.join([key * val for key, val in char_collection]) 
+
+def sort_letters_3(text):
+    """
+    >>> sort_letters('aaabccccdeefffff')
+    'fffffccccaaaeebd'
+    >>> sort_letters('aaadccccbeefffff')
+    'fffffccccaaaeedb'
+    >>> sort_letters('abcdefghijklmnop')
+    'abcdefghijklmnop'
+    >>> sort_letters('')
+    ''
+    >>> sort_letters('aba')
+    'aab'
+    >>> sort_letters('abcabccba')
+    'aaabbbccc'
+    """
+    # with the help of collections.OrderedDict method
+    if not len(text): return text
+    
+    char_dict = OrderedDict();
+    for char in text:
+        val = char_dict[char] if char in char_dict else 0
+        char_dict[char] = val + 1
+    
+    sorted_char_dict = sorted(char_dict.items(), key=lambda char: char[1], reverse=True)
+    
+    return ''.join([key * val for key, val in sorted_char_dict]) 
 
 if __name__ == "__main__":
     import doctest
--- puzzles/zigzag_conversion.py
+++ puzzles/zigzag_conversion.py
@@ -1,40 +1,5 @@
 str1 = 'PAYPALISHIRING'
 str2 = 'DATAROBOTISAWESOME'
-
-def calc_total_cols(rows, stringLen):
-    tot_full_cols = stringLen // rows
-    in_between_cols = rows - 2
-    tot_in_between_cols = in_between_cols * (tot_full_cols - 1)
-    
-    return tot_full_cols + tot_in_between_cols
-
-def build_matrix(size):
-    r, c = size
-    return [['' for col in range(c)] for row in range(r)]
-
-def fill_matrix(matrix, word):
-    current_word_i, r, c, r_lim, c_lim = 0, 0, 0, len(matrix) - 1, len(matrix[0]) - 1
-    
-    while current_word_i < len(word):
-        if r == r_lim:
-            while r > 1:
-                c += 1
-                r -= 1
-                matrix[r][c] = word[current_word_i]
-                current_word_i += 1
-            c += 1
-        elif r == 0:
-
-            while r <= r_lim:
-                if current_word_i == len(word): 
-                    break
-                matrix[r][c] = word[current_word_i]
-                r += 1
-                current_word_i += 1
-        r -= 1
-    
-    return matrix
-    
     
 def convert(s, numRows):
     """
@@ -49,15 +14,17 @@ def convert(s, numRows):
     >>> convert('DATAROBOTISAWESOME', 5)
     'DTMAOIOETBSSAOAERW'
     """
+    res = [''] * numRows
+    step, row = -1, 0
     
-    # suuuper noob solution
-    numCols = calc_total_cols(numRows, len(s))
-    matrix = build_matrix([numRows, numCols])
+    for char in s:
+        res[row] += char
+        # since we change direction when we hit bottom or top, we should change step to opposite
+        if row % (numRows - 1) == 0:
+            step = -step
+        row += step
     
-    temp_res = fill_matrix(matrix, s)
-    
-    final_res = ''.join(''.join(row).strip() for row in temp_res)
-    return final_res
+    return ''.join(res)
 
 
 test1 = convert(str1, 3) # PAHNAPLSIIGYIR
